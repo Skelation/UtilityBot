2ef106fdc63e4c702622ca90e29a86298654fb0f
Heribio <ethan.levratti@icloud.com>
2024-02-13 11:01:51 +0100

    Revert agenda commands

--- cmd-deploy.js
+++ cmd-deploy.js
@@ -1,64 +1,41 @@
 import { REST, Routes, SlashCommandBuilder } from "discord.js";
 import dotenv from "dotenv";
 
-dotenv.config({ path: 'config/.env' });
+dotenv.config({ path: "config/.env" });
 
-const botID = process.env.BOT_ID
-const serverID = process.env.SERVER_ID
-const botToken = process.env.TOKEN
+const botID = process.env.BOT_ID;
+const serverID = process.env.SERVER_ID;
+const botToken = process.env.TOKEN;
 
-const rest = new REST().setToken(botToken)
+const rest = new REST().setToken(botToken);
 const slashRegister = async () => {
-    try {
-        await rest.put(Routes.applicationGuildCommands(botID, serverID), {
-            body: [
-                new SlashCommandBuilder()
-                .setName("ping")
-                .setDescription("Testing connection"),
-
-                new SlashCommandBuilder()
-                .setName("agenda_add")
-                .setDescription("add to your agenda")
-                .addStringOption( option => 
-                    option.setName("date")
-                    .setDescription("Enter a date. FORMAT: DD/MM/YY")
-                    .setRequired(true)
-                )
-                .addStringOption(option => 
-                        option.setName("content")
-                        .setDescription("Enter content")
-                        .setRequired(true)
-                ),
-                new SlashCommandBuilder()
-                .setName("agenda_remove")
-                .setDescription("remove from your agenda")
-                .addIntegerOption( option =>
-                    option.setName("index")
-                    .setDescription("Enter index of item to remove. See the index in /agenda_view")
-                    .setRequired(true)
-                ),
-                new SlashCommandBuilder()
-                .setName("agenda_view")
-                .setDescription("view your agenda"),
-
-                new SlashCommandBuilder()
-                .setName("note_add")
-                .setDescription("add to your notes")
-                .addStringOption( option => 
-                    option.setName("title")
-                    .setDescription("Enter the title of your note")
-                    .setRequired(true))
-                .addStringOption( option =>
-                    option.setName("content")
-                    .setDescription("Enter the body of your note")
-                    .setRequired(true)),
-                
-            ]
-        })
-    } catch (error) {
-    console.log(error)
-    }
-}
+  try {
+    await rest.put(Routes.applicationGuildCommands(botID, serverID), {
+      body: [
+        new SlashCommandBuilder()
+          .setName("ping")
+          .setDescription("Testing connection"),
+        new SlashCommandBuilder()
+          .setName("note_add")
+          .setDescription("add to your notes")
+          .addStringOption((option) =>
+            option
+              .setName("title")
+              .setDescription("Enter the title of your note")
+              .setRequired(true),
+          )
+          .addStringOption((option) =>
+            option
+              .setName("content")
+              .setDescription("Enter the body of your note")
+              .setRequired(true),
+          ),
+      ],
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 slashRegister();
-console.log("Registered slash commands")
+console.log("Registered slash commands");
--- src/index.js
+++ src/index.js
@@ -1,241 +1,93 @@
 import {
-    Client,
-    IntentsBitField,
-    ActivityType,
-    EmbedBuilder,
+  Client,
+  IntentsBitField,
+  ActivityType,
+  EmbedBuilder,
 } from "discord.js";
 
 import dotenv from "dotenv";
 dotenv.config({ path: "config/.env" });
-import fs, { cp } from "fs";
+import fs from "fs";
 
 const DISCORD_TOKEN = process.env.TOKEN;
 
 const client = new Client({
-    intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.MessageContent,
-    ],
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.MessageContent,
+  ],
 });
-
 
 client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    console.log(`${client.user.tag} is utilitying`);
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`${client.user.tag} is utilitying`);
   //Client Activity
-    client.user.setActivity({
-        name: "your problems",
-        type: ActivityType.Listening,
-    });
+  client.user.setActivity({
+    name: "your problems",
+    type: ActivityType.Listening,
+  });
 });
 
-function isValidDate(dateString) {
-    // First check for the pattern
-    if (!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(dateString)) return false;
-
-    // Parse the date parts to integers
-    var parts = dateString.split("/");
-    var day = parseInt(parts[0], 10);
-    var month = parseInt(parts[1], 10);
-    var year = parseInt(parts[2], 10);
-
-    // Check the ranges of month and year
-    if (year < 1000 || year > 3000 || month == 0 || month > 12) return false;
-
-    var monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-    // Adjust for leap years
-    if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
-        monthLength[1] = 29;
-
-    // Check the range of the day
-    return day > 0 && day <= monthLength[month - 1];
-}
-
-//Ping command
 function ping(interaction) {
-    interaction.reply({ content: "Pong!" });
+  interaction.reply("Pong!");
 }
 
-//agenda add command
-function agenda_add(interaction) {
-    const filePath = "data.json";
-    let data = {};
-
-    try {
-        //Setup json file
-        if (fs.existsSync(filePath)) {
-            const rawData = fs.readFileSync(filePath);
-            if (rawData.length > 0) {
-                data = JSON.parse(rawData);
-            }
-        }
-        //If user doesn't exist then create an empty array
-        if (!data[interaction.user.id]) {
-            data[interaction.user.id] = [];
-        }
-
-        const date = interaction.options.getString("date");
-        const content = interaction.options.getString("content");
-        //Check if date is valid
-        if (isValidDate(date)) {
-            data[interaction.user.id].push({ date: date, content: content });
-            fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
-
-            const embed = new EmbedBuilder()
-                .setTitle("Agenda")
-                .setDescription(`Added "${content}" to your agenda at ${date}`)
-                .setTimestamp();
-            interaction.channel.send({ embeds: [embed] });
-        } else {
-            console.log(isValidDate(date));
-            interaction.channel.send("Invalid date format!");
-        }
-    } catch (error) {
-        console.log(error);
-    }
-}
-// Agenda View Command
-function agenda_view(interaction) {
-    const filePath = "data.json";
-    let data = {};
-
-    try {
-        if (fs.existsSync(filePath)) {
-            const rawData = fs.readFileSync(filePath);
-            if (rawData.length > 0) {
-                data = JSON.parse(rawData);
-            }
-        }
-        if (!data[interaction.user.id]) {
-            data[interaction.user.id] = [];
-        }
-        if (data[interaction.user.id].length > 0) {
-            for (let i = 0; i < data[interaction.user.id].length; i++) {
-                const date = data[interaction.user.id][i]["date"];
-                const content = data[interaction.user.id][i]["content"];
-                let dateEvent = `${i + 1}. ${date} ${content}`;
-
-                const embed = new EmbedBuilder()
-                    .setTitle("Agenda")
-                    .setColor("#0099ff")
-                    .setTimestamp()
-                    .setDescription(dateEvent);
-                interaction.channel.send({ embeds: [embed] });
-            }
-        } else {
-            interaction.channel.send("You don't have any agenda items!");
-        }
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-//Agenda Delete Command
-function agenda_delete(interaction) {
-    const filePath = "data.json";
-    let data = {};
-    try {
-        if (fs.existsSync(filePath)) {
-            const rawData = fs.readFileSync(filePath);
-            if (rawData.length > 0) {
-                data = JSON.parse(rawData);
-            }
-        }
-        if (!data[interaction.user.id]) {
-            data[interaction.user.id] = [];
-        }
-        if (data[interaction.user.id].length > 0) {
-            const index = interaction.options.getInteger("index");
-            if (index > 0 && index <= data[interaction.user.id].length) {
-                data[interaction.user.id].splice(index - 1, 1);
-                fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
-                const embed = new EmbedBuilder()
-                    .setTitle("Agenda")
-                    .setColor("#0099ff")
-                    .setTimestamp()
-                    .setDescription(`Deleted agenda item ${index}`);
-                interaction.channel.send({ embeds: [embed] });
-            } else {
-                interaction.channel.send("Invalid index!");
-            }
-        }
-    } catch (error) {
-        console.log(error);
-    }
-}
 //Note add command
 function note_add(interaction) {
-    const filePath = "note_data.json";
-    let data = {};
+  const filePath = "note_data.json";
+  let data = {};
 
-    try {
-        //Setup json file incase file isn't there
-        if (fs.existsSync(filePath)) {
-            const rawData = fs.readFileSync(filePath);
-            if (rawData.length > 0) {
-                data = JSON.parse(rawData);
-            }
-        }
-        //If user doesn't exist then create an empty array
-        if (!data[interaction.user.id]) {
-            data[interaction.user.id] = [];
-        }
-
-        const date = interaction.options.getString("title");
-        const content = interaction.options.getString("content");
-            data[interaction.user.id].push({ date: date, content: content });
-            fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
-
-            const embed = new EmbedBuilder()
-                .setTitle("Notes")
-                .setDescription(`Added "${content}" to your notes called ${Title}`)
-                .setTimestamp()
-            interaction.channel.send({ embeds: [embed] });
-        }
-     catch (error) {
-        console.log(error);
+  try {
+    //Setup json file incase file isn't there
+    if (fs.existsSync(filePath)) {
+      const rawData = fs.readFileSync(filePath);
+      if (rawData.length > 0) {
+        data = JSON.parse(rawData);
+      }
     }
+    //If user doesn't exist then create an empty array
+    if (!data[interaction.user.id]) {
+      data[interaction.user.id] = [];
+    }
+
+    const date = interaction.options.getString("title");
+    const content = interaction.options.getString("content");
+    data[interaction.user.id].push({ date: date, content: content });
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+
+    const embed = new EmbedBuilder()
+      .setTitle("Notes")
+      .setDescription(`Added "${content}" to your notes called ${Title}`)
+      .setTimestamp();
+    interaction.channel.send({ embeds: [embed] });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 //Slash Commands
 client.on("interactionCreate", async (interaction) => {
-    console.log(interaction.commandName);
-    if (interaction.isCommand()) {
-        try {
-            if (interaction.commandName === "ping") {
-                await ping(interaction);
-            }
-            if (interaction.commandName === "agenda_add") {
-                await interaction.deferReply();
-                await interaction.deleteReply();
-                await agenda_add(interaction);
-            }
-            if (interaction.commandName === "agenda_view") {
-                await interaction.deferReply();
-                await interaction.deleteReply();
-                await agenda_view(interaction);
-            }
-            if (interaction.commandName === "agenda_remove") {
-                await interaction.deferReply();
-                await interaction.deleteReply();
-                await agenda_delete(interaction);
-            }
-            if (interaction.commandName === "note_add") {
-                await interaction.deferReply();
-                await interaction.deleteReply();
-                await agenda_delete(interaction);
-            }
-        } catch (error) {
-            console.error(error);
-            await interaction.reply({
-                content: "There was an error while executing this command!",
-                ephemeral: true,
-            });
-        }
+  console.log(interaction.commandName);
+  if (interaction.isCommand()) {
+    try {
+      if (interaction.commandName === "ping") {
+        ping(interaction);
+      }
+      if (interaction.commandName === "note_add") {
+        await interaction.deferReply();
+        await interaction.deleteReply();
+        note_add(interaction);
+      }
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
     }
+  }
 });
 
 client.login(DISCORD_TOKEN);
